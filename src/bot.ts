b35146f804feae5efc7d16ba2f70e996db4fa0c5
christopher-r-doucet <cdoucet6302@gmail.com>
2023-02-27 18:55:15 -0700

    last one for tonight

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,4 @@
 import { Bot, webhookCallback } from "grammy";
-import { run } from "@grammyjs/runner";
 import express from 'express';
 
 // Create a bot using the Telegram token
@@ -389,19 +388,6 @@ bot.api.setMyCommands([
 ]);
 
 // Start the server
-
-/*if (process.env.NODE_ENV === "production") { 
-  // Use Webhooks for the production server
   const app = express();
   app.use(express.json());
   app.use(webhookCallback(bot, "express"));
-  
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Bot listening on port ${PORT}`);
-  });
-} else {
-  // Use Long Polling for development
-  bot.start();
-}*/
-run(bot);
